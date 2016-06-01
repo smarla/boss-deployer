d2edec996c0c23b0cc33a1e4d3f8e58886c1ab43
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-06-02 00:54:55 +0200

    More on raspAPI - Including base structure for cd methods.

--- raspberry/index.js
+++ raspberry/index.js
@@ -15,32 +15,39 @@ app.get('/lock', function (req, res) {
         port: '80'
     }).end();
 
+    locked = true;
+
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ operation: 'lock', status: 'ok' }));
 });
 
 app.get('/free', function (req, res) {
-    http.request({
-        host: '192.168.0.254',
-        path: '/free',
-        method: 'GET',
-        port: '80'
-    }).end();
-
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ operation: 'free', status: 'ok' }));
+    locked = false;
 });
 
-app.get('/check', function (req, res) {
-    http.request({
-        host: '192.168.0.254',
-        path: '/free',
-        method: 'GET',
-        port: '80'
-    }).end();
+app.get('/bypass', function (req, res) {
+    bypass = true;
+    disabled = false;
+});
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ operation: 'free', status: 'ok' }));
+app.get('/disable', function (req, res) {
+    bypass = false;
+    disabled = true;
+});
+
+/*
+ * Continuous delivery things
+ */
+app.get('/cd-check', function (req, res) {
+    // Check versions
+});
+
+app.get('/cd-in', function (req, res) {
+    // Check versions
+});
+
+app.get('/cd-out', function (req, res) {
+    // Check versions
 });
 
 var server = app.listen(5000, function () {
